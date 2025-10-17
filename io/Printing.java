5f1fa2c7cfd0d598ecb7bfee4322961ac59519f1
I-Berard <irakozeberard12@gmail.com>
2025-10-17 15:50:45 +0200

    did some printf

---
+++ io/Printing.java
@@ -0,0 +1,8 @@
+public class Printing {
+    public static void main(String[] args) {
+        System.out.printf("%b%n", 7<6);
+        System.out.printf("%-10s | %-5s%n", "Name", "Age");
+        System.out.println("-".repeat(21));
+        System.out.printf("%-10s | %-5d%n", "Berard", 17);
+    }
+}
