2af1c1022e189cc42dfd2fe0a75c1fe360fe6bcd
I-Berard <irakozeberard12@gmail.com>
2025-11-17 21:47:25 +0200

    Made a few notes for the simple downloader

--- Download_manager/SimpleDownloader.java
+++ Download_manager/SimpleDownloader.java
@@ -7,16 +7,16 @@ public class SimpleDownloader {
         String outputFile = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
         // System.out.println(outputFile);
 
-        URL url = new URL(fileUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        InputStream in = connection.getInputStream();
-        FileOutputStream out = new FileOutputStream(outputFile);
+        URL url = new URL(fileUrl); // This allows you to create a url instance, it provides different methodologies for different things
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); // Here you are making a connection to the server and casting it to an HTTP cause you know that it is already http(s)
+        InputStream in = connection.getInputStream(); // This is a method that allows you to get the data from the server
+        FileOutputStream out = new FileOutputStream(outputFile); // Open an outputstream
 
         byte[] buffer = new byte[4096];
         int bytesRead;
 
-        while((bytesRead = in.read(buffer)) != -1){
-            out.write(buffer, 0, bytesRead);
+        while((bytesRead = in.read(buffer)) != -1){ // read and return the value of bytes read
+            out.write(buffer, 0, bytesRead); // write bytes from 0 to bytesRead to file
         }
 
         in.close();
