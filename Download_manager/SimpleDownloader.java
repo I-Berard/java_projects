567107f1ea3abf695f0bf83d8930feea21db1ada
I-Berard <irakozeberard12@gmail.com>
2025-11-17 21:35:33 +0200

    Made a minimalistic download manager

--- Download_manager/SimpleDownloader.java
+++ Download_manager/SimpleDownloader.java
@@ -1,11 +1,26 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public class SimpleDownloader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         String fileUrl = "https://raw.githubusercontent.com/github/explore/main/topics/java/java.png";
         String outputFile = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
-        System.out.println(outputFile);
+        // System.out.println(outputFile);
+
+        URL url = new URL(fileUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream in = connection.getInputStream();
+        FileOutputStream out = new FileOutputStream(outputFile);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+
+        while((bytesRead = in.read(buffer)) != -1){
+            out.write(buffer, 0, bytesRead);
+        }
+
+        in.close();
+        out.close();
+        System.out.println("Download complete");
     }
 }
