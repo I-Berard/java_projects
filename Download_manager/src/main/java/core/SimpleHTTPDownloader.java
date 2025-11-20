53be729e8d4026f8f3ed27690dfb06bc951e54a8
I-Berard <irakozeberard12@gmail.com>
2025-11-20 21:57:22 +0200

    Update src/main/java/core/SimpleHTTPDownloader.java

--- Download_manager/src/main/java/core/SimpleHTTPDownloader.java
+++ Download_manager/src/main/java/core/SimpleHTTPDownloader.java
@@ -1,37 +1,71 @@
 package core;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.URI;
-// import java.nio.file.Files;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-class SimpleHTTPDownloader {
-    public static void downloadFile(String fileUrl) throws Exception {
-        String output = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
-        // System.out.println(output);
+public class SimpleHTTPDownloader {
 
+    public static void downloadFile(String fileUrl, app.models.DownloadItem item) throws Exception {
+        String output = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
         Path outputPath = Paths.get(System.getProperty("user.home"), "Downloads", output);
-        // System.out.println(outputPath);
 
         HttpClient client = HttpClient.newHttpClient();
 
-        HttpRequest request = HttpRequest.newBuilder()
+        // HEAD request to get file size
+        HttpRequest headRequest = HttpRequest.newBuilder()
+            .uri(new URI(fileUrl))
+            .method("HEAD", HttpRequest.BodyPublishers.noBody())
+            .build();
+
+        HttpResponse<Void> headResponse = client.send(headRequest, HttpResponse.BodyHandlers.discarding());
+        long fileSize = Long.parseLong(headResponse.headers().firstValue("Content-Length").orElse("0"));
+
+        item.setFileName(output);
+        item.setStatus("Downloading...");
+
+        // GET request
+        HttpRequest downloadRequest = HttpRequest.newBuilder()
             .uri(new URI(fileUrl))
             .GET()
             .build();
-        
-        HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(outputPath));
 
-        System.out.println("Downloaded to: " + response.body());
+        HttpResponse<InputStream> response =
+            client.send(downloadRequest, HttpResponse.BodyHandlers.ofInputStream());
+
+        try (InputStream in = response.body();
+             OutputStream out = Files.newOutputStream(outputPath)) {
+
+            byte[] buffer = new byte[8192];
+            long totalRead = 0;
+            int bytesRead;
+
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+                totalRead += bytesRead;
+
+                double progress = (double) totalRead / fileSize;
+                item.setProgress(progress);   // UI will auto-update if using Observable properties
+            }
+        }
+
+        item.setStatus("Completed");
+        item.setProgress(1.0);
     }
 
     public static void main(String[] args) {
-        try{
-            downloadFile("https://raw.githubusercontent.com/github/explore/main/topics/java/java.png");
-        }catch(Exception e){
+        try {
+            downloadFile(
+                "https://raw.githubusercontent.com/github/explore/main/topics/java/java.png",
+                new app.models.DownloadItem(null)
+            );
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
