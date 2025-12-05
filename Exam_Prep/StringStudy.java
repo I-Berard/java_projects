f83a0e67bbb99aa59ea82afefaa1d62fc8f78365
I-Berard <irakozeberard12@gmail.com>
2025-12-05 08:44:58 +0200

    Added some of the good stuff

---
+++ Exam_Prep/StringStudy.java
@@ -0,0 +1,10 @@
+public class StringStudy {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+
+        String s3 = new String("hello");
+
+        System.out.println(s1.equals(s2));
+    }
+}
